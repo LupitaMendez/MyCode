3a0ec20a5c1d8f9d16e12c6c57b3b5f45a82a7f8
Guadalupe Méndez <guadalupemendez@MacBook-Air-de-Guadalupe.local>
2021-09-22 23:37:57 -0500

    Frase Albert Einstein

---
+++ Frase_Albert_Einstein.py
@@ -0,0 +1,7 @@
+
+Frase= "      \"Hay dos formas de ver la vida:\n   una es creer que no existen milagros,\n   la otra es creer que todo es un milagro.\" "
+Autor= "               Albert Einstein"
+
+print (Frase) 
+print (Autor.upper())
+
