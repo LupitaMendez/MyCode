8d43ff297442bf21e6abecb7022f45f29e9e7ecf
Guadalupe Méndez <guadalupemendez@MacBook-Air-de-Guadalupe.local>
2021-09-22 23:15:25 -0500

    Segundo ejercicio nombre completo

---
+++ nombre_completo_Mym.py
@@ -0,0 +1,6 @@
+Nombres= "Guadalupe de la Paz"
+Apellido_Paterno= "Méndez"
+Apellido_Materno= "Pelayo"
+Nombre_Completo = f" {Nombres} {Apellido_Paterno} {Apellido_Materno}"
+print(Nombre_Completo.lower())
+print(Nombre_Completo.upper())
