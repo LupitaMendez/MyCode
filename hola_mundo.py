57bf2ecbee90a2f40975fa89e1ddcc6111c97864
Guadalupe Méndez <guadalupemendez@MacBook-Air-de-Guadalupe.local>
2021-09-22 22:42:34 -0500

    Mi primer programa

---
+++ hola_mundo.py
@@ -0,0 +1,20 @@
+#message1= "Hola mundo"
+#print(message1.lower())
+
+#message2 = "adios mundo"
+#print(message2.upper())
+
+#nombre= "Lupita"
+#apellido= "Méndez"
+
+#nombre_completo= nombre + " " + apellido
+#print (nombre_completo)
+
+#nombre_completo_f_string = f"{nombre} {apellido}"
+#print (nombre_completo_f_string)
+
+#texto = "uno\ndos\ntres"
+#print (texto)
+
+mi_mensaje= "hola    .   "
+print(mi_mensaje.strip())
