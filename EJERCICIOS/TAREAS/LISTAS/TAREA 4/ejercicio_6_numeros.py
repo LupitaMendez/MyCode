d6dcaef79062bf23b8126dc917d83bff559a7bd1
Guadalupe Méndez <guadalupemendez@MacBook-Air-de-Guadalupe.local>
2021-10-13 23:08:34 -0500

    modificación del ejercicio 6

--- EJERCICIOS/TAREAS/LISTAS/TAREA 4/ejercicio_6_numeros.py
+++ EJERCICIOS/TAREAS/LISTAS/TAREA 4/ejercicio_6_numeros.py
@@ -1,9 +1,20 @@
+"""Crear una lista e invertir esa lista usando while o for"""
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+longitud = len(numeros)-1
 
-numeros = [0,1,2,3,4,5,6,7,8,9,10]
+for i in range(longitud + 1):
+      #print(numeros[longitud-i])
+      numero = numeros[longitud-i]
+      #numeros.insert(numero,0)
+      
+      print(numero)
+#print(numeros)
 
-for value in reversed(numeros):
-    numeros=value
-    print(numeros)
+
+
+
+
+
 
 
 
